b8335a754640b38c95e20206bee6e6b068898d3c
Steve Kane <quantumkane00@gmail.com>
2021-03-08 22:49:29 -0600

    should be done

--- prepare.py
+++ prepare.py
@@ -22,6 +22,10 @@ def telco_prep(df):
     Return train, validate, test dfs.
     '''
     # Changing features to 0 for no and 1 for yes
+    df['partner'] = df['partner'].replace({'No': 0, 'Yes': 1})
+    df['dependents'] = df['dependents'].replace({'No': 0, 'Yes': 1})
+    df['phone_service'] = df['phone_service'].replace({'No': 0, 'Yes': 1})
+    df['paperless_billing'] = df['paperless_billing'].replace({'No': 0, 'Yes': 1})
     df['streaming_movies'] = df.streaming_movies.replace({'No internet service': 0, 'No': 0, 'Yes': 1})
     df['streaming_tv'] = df.streaming_tv.replace({'No internet service': 0, 'No': 0, 'Yes': 1})
     df['online_security'] = df.online_security.replace({'No internet service': 0, 'No': 0, 'Yes': 1})
@@ -30,8 +34,16 @@ def telco_prep(df):
     df['tech_support'] = df.tech_support.replace({'No internet service': 0, 'No': 0, 'Yes': 1})
     df['churn'] = df.churn.replace({'No': 0, 'Yes': 1})
     
+    # Maintain original columns for dummy vars
+    df['internet_service_type_id_orig'] = df['internet_service_type_id']
+    df['online_security_orig'] = df['online_security']
+    df['tech_support_orig'] = df['tech_support']
+    
+    # Create dummy vars for columns.
+    df = pd.get_dummies(df, columns=['internet_service_type_id', 'online_security', 'tech_support'], drop_first=[True, True, True])
+    
     # Dropping unnecessary columns
-    df = df.drop(['total_charges'],axis=1)
+    df = df.drop(['total_charges', 'gender', 'senior_citizen'],axis=1)
     
     # Prepping tenure columns
     # Renaming tenure to tenure_months before creating a tenure_years column
